7fdfee4c5ec2d2721b1742b4484f2d7acb4bd0e4
Stalder <serg_lavinov@inbox.ru>
2019-03-25 23:43:26 +0300

    Header

--- article.php
+++ article.php
@@ -3,18 +3,16 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Новостной портал</title>
+    <title>*Название статьи*</title>
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/styles.css"/>
 </head>
 <body>
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-$txt = "Lol, really?";
-echo "<h2>" . $txt . "</h2>"
-?>
+<div class="container">
+    <header class="row blue darken-3 z-depth-2 section header">
+        <h1 class="grey-text text-lighten-4">Новостной портал</h1>
+    </header>
+</div>
 <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
