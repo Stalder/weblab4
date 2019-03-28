bec9a389126ed037f1e444efdbeabb1db6a35220
Stalder <serg_lavinov@inbox.ru>
2019-03-28 11:57:47 +0300

    Added dummies for article components

--- components.php
+++ components.php
@@ -2,10 +2,32 @@
 
 function renderHeader($title)
 {
-    $headerHeredoc = <<<EOT
+    $headerHeredoc = <<<HTML
         <header class="row blue darken-3 z-depth-2 section header">
             <h1 class="grey-text text-lighten-4">$title</h1>
         </header>
-EOT;
+HTML;
+    echo $headerHeredoc;
+}
+
+function renderArticlePreview($title, $text, $date)
+{
+    $headerHeredoc = <<<HTML
+        <div>
+            <h3>$title</h3>
+            <p>$text</p>
+        </div>
+HTML;
+    echo $headerHeredoc;
+}
+
+function renderArticleForAdmin($title, $date)
+{
+    $headerHeredoc = <<<HTML
+        <div>
+            <h3>$title</h3>
+            <p>$date</p>
+        </div>
+HTML;
     echo $headerHeredoc;
 }
